f1ecf0584e79c980c5f8e152b92a11f5951c8f0a
Nille <nille0722@gmail.com>
2026-06-02 18:46:26 -0300

    feat: adicionar Adotante.java e AdotanteTest.java

--- src/domain/Adotante.java
+++ src/domain/Adotante.java
@@ -1,25 +1,22 @@
-package src.domain;
+package domain;
 
-public class Adotante {
-        private String nome;
-        private String idade;
-        private String email;
-
-        public Adotante(String nome, String idade, String email){
-            this.nome=nome;
-            this.idade=idade;
-            this.email=email;
-        }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getIdade() {
-        return idade;
-    }
-}
+public class Adotante{
+      private String nome;
+       private CPF cpf;
+         private Endereco endereco;
+    public Adotante(String nome, CPF cpf, Endereco endereco){
+        if(nome == null || nome.isBlank()){
+            throw new IllegalArgumentException("Nome do indíviduo obrigatório");}
+         if(cpf == null){
+            throw new IllegalArgumentException("CPF do indíviduo obrigatório");}
+         if (endereco == null){
+            throw new IllegalArgumentException("Endereço do indíviduo obrigatório");}
+         this.nome = nome;
+           this.cpf = cpf;
+        this.endereco = endereco;}
+     public String getNome(){
+        return nome;}
+      public CPF getCpf(){
+        return cpf;}
+     public Endereco getEndereco(){
+        return endereco;}}
--- test/AdotanteTest.java
+++ test/AdotanteTest.java
@@ -1,4 +1,45 @@
 package test;
-
-public class AdotanteTest {
-}
+import domain.Adotante;
+import domain.CPF;
+import domain.Endereco;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class AdotanteTest{
+    @Test
+    void deveCriarAdotanteValido(){
+        CPF cpf = new CPF("87945601252");
+        Endereco endereco =
+                new Endereco(
+                        "Rua L",
+                        "Bahia",
+                        "BA",
+                        "03000-000"
+                );
+                Adotante adotante =
+                new Adotante(
+                        "Joyce",
+                        cpf,
+                        endereco
+                );
+        assertEquals("Joyce", adotante.getNome());}
+    @Test
+    void deveRejeitarCpfInvalido(){
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new CPF("879")
+        );}
+    @Test
+    void deveRejeitarNomeVazio(){
+        CPF cpf = new CPF("87945601252");
+                Endereco endereco =
+                new Endereco(
+                        "Rua L",
+                        "Bahia",
+                        "BA",
+                        "03000-000"
+                );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Adotante("", cpf, endereco)
+        );
+    }}
