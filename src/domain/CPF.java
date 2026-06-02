d17da0777508a11d44401dff12d8fb99afb1880c
Nille <nille0722@gmail.com>
2026-06-02 19:02:59 -0300

    feat: Nivia adiciona CPF.java e Endereco.java

---
+++ src/domain/CPF.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class CPF{
+    private String numero;
+    public CPF(String numero){
+        if (numero == null || numero.length() != 11){
+            throw new IllegalArgumentException("CPF não correspondente");}
+        this.numero = numero;}
+    public String getNumero(){
+        return numero;}}
