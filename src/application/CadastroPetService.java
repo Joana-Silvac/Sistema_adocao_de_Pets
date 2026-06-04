2ee597c7830a17d0698faeb5f27addc6ef062bff
Crislany Victória <crislanyviictoria@gmail.com>
2026-06-04 13:54:46 -0300

    implementando classe responsável por cadastro dos pets

--- src/application/CadastroPetService.java
+++ src/application/CadastroPetService.java
@@ -1,4 +1,23 @@
-package src.application;
+package application;
+import domain.Pet;
+import infrastructure.PetRepository;
 
 public class CadastroPetService {
-}
+
+        private PetRepository petRepository;
+
+        public CadastroPetService(PetRepository petRepository) {
+            this.petRepository = petRepository;
+        }
+
+        public void cadastrarPet(Pet pet) {
+
+            if (pet == null) {
+                throw new IllegalArgumentException(
+                        "Informe os dados do pet para continuar o cadastro."
+                );
+            }
+
+            petRepository.salvar(pet);
+        }
+    }
