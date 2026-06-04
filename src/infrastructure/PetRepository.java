f6b7602438237e4916cf5a5a434b413981bc02cf
Crislany Victória <crislanyviictoria@gmail.com>
2026-06-04 14:03:16 -0300

    ajustei para as classes de application funcionarem

--- src/infrastructure/PetRepository.java
+++ src/infrastructure/PetRepository.java
@@ -1,4 +1,9 @@
-package src.infrastructure;
+package infrastructure;
+
+import domain.Pet;
 
 public class PetRepository {
-}
+        public void salvar(Pet pet) {
+            System.out.println("Pet salvo: " + pet.getNome());
+        }
+    }
