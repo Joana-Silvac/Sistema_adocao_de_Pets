82ab74297337002ec2f8c43953779c1dda72fceb
Laura Pontes <lauraangell3003@gmail.com>
2026-06-04 21:39:13 -0300

    feat: adicionei validao de email ao adotante

--- src/domain/Adotante.java
+++ src/domain/Adotante.java
@@ -2,21 +2,29 @@ package domain;
 
 public class Adotante{
       private String nome;
-       private CPF cpf;
-         private Endereco endereco;
-    public Adotante(String nome, CPF cpf, Endereco endereco){
+      private CPF cpf;
+      private Endereco endereco;
+      private Email email;
+
+    public Adotante(String nome, CPF cpf, Endereco endereco, Email email){
         if(nome == null || nome.isBlank()){
             throw new IllegalArgumentException("Nome do indíviduo obrigatório");}
          if(cpf == null){
             throw new IllegalArgumentException("CPF do indíviduo obrigatório");}
          if (endereco == null){
             throw new IllegalArgumentException("Endereço do indíviduo obrigatório");}
-         this.nome = nome;
-           this.cpf = cpf;
-        this.endereco = endereco;}
+        if (email == null) {
+            throw new IllegalArgumentException("Email do indivíduo obrigatório");}
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.email = email;}
      public String getNome(){
         return nome;}
       public CPF getCpf(){
         return cpf;}
      public Endereco getEndereco(){
-        return endereco;}}
+        return endereco;}
+public Email getEmail(){
+    return email;}
+}
