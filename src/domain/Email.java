e1d3db3c8c089fd950a26fb7bdaf07313fd1ec5d
Laura Pontes <lauraangell3003@gmail.com>
2026-06-04 21:25:07 -0300

    feat: implementar Email

--- src/domain/Email.java
+++ src/domain/Email.java
@@ -1,4 +1,33 @@
 package domain;
 
 public class Email {
+
+    private String endereco;
+
+    public Email(String endereco) {
+
+        if (endereco == null || endereco.isBlank()) {
+            throw new IllegalArgumentException("Email é obrigatório!");
+        }
+
+        if (!endereco.contains("@")) {
+            throw new IllegalArgumentException("Email inválido!");
+        }
+
+        String[] partes = endereco.split("@");
+
+        if (partes.length != 2) {
+            throw new IllegalArgumentException("Email inválido!");
+        }
+
+        if (partes[0].isBlank() || partes[1].isBlank()) {
+            throw new IllegalArgumentException("Email inválido!");
+        }
+
+        this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
 }
